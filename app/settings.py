f4d2bd633c22609fccb810994f85cf61bbb8103f
MarlineKhavele <marlinekhavele6@gmail.com>
2020-12-03 11:44:41 +0300

    Added schema

--- app/settings.py
+++ app/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
+    "base",
+    "graphene_django",
+
 ]
+
+GRAPHENE = {
+    "SCHEMA": "app.schema.schema"
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
