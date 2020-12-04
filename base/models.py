7e83b9f43909c66dc7da461e483fd7e8d585279f
MarlineKhavele <marlinekhavele6@gmail.com>
2020-12-04 17:52:34 +0300

    changed model

--- base/models.py
+++ base/models.py
@@ -1,29 +1,51 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
+from decimal import Decimal
+
+
 
 
 # Create your models here.
+class Issue(models.Model):
+    """issue model"""
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    def ___str___(self):
+        return self.name
+
+
 class Location(models.Model):
     """Location Model"""
     name = models.CharField(max_length=100)
+    description= models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
     def ___str___(self):
         return self.name
 
 class Prescription(models.Model):
     """ prescription Model"""
     name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+
 
     def ___str___(self):
         return self.name
 
-class Customer(models.Model):
-    """ Customer Model"""
-    name = models.CharField(max_length=100)        
-    location = models.ForeignKey(Location,on_delete=models.CASCADE,related_name="location")
-    prescription = models.ForeignKey(Prescription,on_delete=models.CASCADE,related_name="prescription")
-    # receipt = models.ForeignKey(Receipt,on_delete=models.CASCADE,related_name="receipt")
-    # order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name="order")
-    satisfaction = models.TextField()
+class Patient(models.Model):
+    """ Patient Model"""
+    first_name = models.CharField(max_length=100) 
+    last_name = models.CharField(max_length=100)
+    age = models.CharField(max_length=100)  
+    phone_number = models.IntegerField() 
+    id_number = models.IntegerField() 
+    next_kin = models.IntegerField()       
+    satisfaction = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
@@ -35,41 +57,44 @@ class Receipt(models.Model):
     location = models.ForeignKey(Location,on_delete=models.CASCADE,related_name="location")
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE,related_name="customer")
     prescription = models.ForeignKey(Prescription,on_delete=models.CASCADE,related_name="prescription")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
     def ___str___(self):
         return self.name
 
-class Order(models.Model):
-    """Order Model"""
-    name = models.CharField(max_length=100)
-    prescription = models.ForeignKey(Prescription,on_delete=models.CASCADE,related_name="prescription")
-    customer = models .ForeignKey(Customer,on_delete=models.CASCADE,related_name="customer")
-    location = models.ForeignKey(Location,on_delete=models.CASCADE,related_name="location")
-    receipt = models.ForeignKey(Receipt,on_delete=models.CASCADE,related_name="receipt")
-    def ___str___(self):
-        return self.name
 
 
 class Staff(models.Model):
     """ Staff Model"""
-    name = models.CharField(max_length=100)
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
     def ___str___(self):
         return self.name
 
-class KeyIssues(models.Model):
-    """ Key Issues Model"""
-    name = models.CharField(max_length=100)
-    customer = models .ForeignKey(Customer,on_delete=models.CASCADE,related_name="customer")
+class Visit(models.Model):
+    patient = models.ForeignKey(Patient,on_delete=models.CASCADE,related_name="patient")
     location = models.ForeignKey(Location,on_delete=models.CASCADE,related_name="location")
+    staff = models.ForeignKey(Staff,on_delete=models.CASCADE,related_name="staff")
+    Issue= models.ManyToManyField()
+    nps = models.IntegerField()
+    cost = models.DecimalField(max_digits=12, decimal_places=2, default=Decimal("0.00"))
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+
     def ___str___(self):
-        return self.name
+        return self.nps
 
 
 
 
 
 
-    
+
+
+
 
 
 
