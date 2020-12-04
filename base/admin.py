fd2809d9eec14a32e65da446eeda261347c3cbcf
MarlineKhavele <marlinekhavele6@gmail.com>
2020-12-04 22:07:51 +0300

    updating models

--- base/admin.py
+++ base/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import (Issue,Location,Prescription,Patient,Staff,Visit)
+from .models import (Issue,Location,Patient,Staff,Visit)
 
 
 # Register your models here.
 admin.site.register(Issue)
 admin.site.register(Location)
-admin.site.register(Prescription)
 admin.site.register(Patient)
 admin.site.register(Staff)
 admin.site.register(Visit)
--- base/models.py
+++ base/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 from datetime import datetime
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
 from decimal import Decimal
 
 # Create your models here.
+class Receipt(models.Model):
+    """receipt model"""
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def ___str___(self):
+        return self.name
+
+class Prescription(models.Model):
+    """ prescription model"""
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def ___str___(self):
+        return self.name
+
+
 class Issue(models.Model):
     """issue model"""
     name = models.CharField(max_length=100)
+    receipt = models.ForeignKey(Receipt,on_delete=models.CASCADE,related_name="receipt")
+    prescription = models.ForeignKey(Prescription,on_delete=models.CASCADE,related_name="prescription")
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
     def ___str___(self):
@@ -23,15 +47,6 @@ class Location(models.Model):
     def ___str___(self):
         return self.name
 
-class Prescription(models.Model):
-    """ prescription Model"""
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False)
-
-    def ___str___(self):
-        return self.name
-
 class Patient(models.Model):
     """ Patient Model"""
     first_name = models.CharField(max_length=100) 
@@ -39,7 +54,7 @@ class Patient(models.Model):
     age = models.CharField(max_length=100)  
     phone_number = models.IntegerField() 
     id_number = models.IntegerField() 
-    next_kin = models.IntegerField()       
+    next_kin = models.TextField()       
     satisfaction = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
@@ -47,27 +62,44 @@ class Patient(models.Model):
     def ___str___(self):
         return self.first_name
 
-class Staff(models.Model):
-    """ Staff Model"""
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False)
-    def ___str___(self):
-        return self.user
+
+class Staff(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    is_active = models.BooleanField(_('active'), default=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+    def get_short_name(self):
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
+
 
 class Visit(models.Model):
     patient = models.ForeignKey(Patient,on_delete=models.CASCADE,related_name="patient")
     location = models.ForeignKey(Location,on_delete=models.CASCADE,related_name="location")
     staff = models.ForeignKey(Staff,on_delete=models.CASCADE,related_name="staff")
-    Issue= models.ManyToManyField()
+    issues = models.ManyToManyField()
     nps = models.IntegerField()
     cost = models.DecimalField(max_digits=12, decimal_places=2, default=Decimal("0.00"))
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
-
-
     def ___str___(self):
-        return self.nps
+        return "{} visit at {}".format(self.patient.name, self.location.name)
 
 
 
