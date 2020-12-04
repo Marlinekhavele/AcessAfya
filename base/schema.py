902da68a2ace4911a499d93907e42f1c5323feff
MarlineKhavele <marlinekhavele6@gmail.com>
2020-12-04 19:06:41 +0300

    house cleaning

--- base/schema.py
+++ base/schema.py
@@ -1,3 +1,3 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import (Location,Prescription,Customer)
+from .models import (Issue,Location,Prescription,Patient,Staff,Visit)
