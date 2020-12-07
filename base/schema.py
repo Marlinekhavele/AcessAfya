ff69540c50cded725ff7b4c75a697480995e86c9
MarlineKhavele <marlinekhavele6@gmail.com>
2020-12-07 14:44:53 +0300

    schema working

--- base/schema.py
+++ base/schema.py
@@ -1,16 +1,38 @@
 import graphene
 from graphene_django.types import DjangoObjectType, ObjectType
-from .models import (Issue,Location,Patient,Staff,Visit)
+from .models import (Receipt,Prescription,Issue,Location,Patient,Staff,Visit)
+
+
+
+class ReceiptType(DjangoObjectType):
+    class Meta:
+        model = Receipt
+        fields = ("id", "name", "created_at","updated_at")
+
+class PrescriptionType(DjangoObjectType):
+    class Meta:
+        model = Prescription
+        fields = ("id", "name", "created_at","updated_at")
 
 class IssueType(DjangoObjectType):
     class Meta:
         model = Issue
-        fields = ("id", "name", "created_at","updated_at")
+        fields = ("id", "name", "receipt","prescription","created_at","updated_at")
 
 class Query(graphene.ObjectType):
-    all_issue = graphene.List(IssueType)
+    receipt = graphene.List(ReceiptType)
+    prescription = graphene.List(PrescriptionType)
+    issue = graphene.List(IssueType)
 
-    def resolve_all_issue(root,info):
+
+    def resolve_receipt(root,info):
+        return Receipt.objects.all()
+    
+    def resolve_prescription(root,info):
+        return Prescription.objects.all()
+
+
+    def resolve_issue(root,info):
         return Issue.objects.all()
 
 schema = graphene.Schema(query=Query)
