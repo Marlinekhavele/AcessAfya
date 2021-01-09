fcda8992684244303b7aaf9df080b0abc73e2077
MarlineKhavele <marlinekhavele6@gmail.com>
2021-01-09 22:11:41 +0300

    Added python decouple

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=oaf8^c0w&vhxy_4j_3-1tu!&#04vx+l2%!mn!)0y&jlplt_l!'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = ["marline.pythonanywhere.com","127.0.0.1"]
 
@@ -84,10 +86,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "afya",
-        "USER": "postgres",
-        "PASSWORD": "password",
-        "HOST": "localhost",
+        "NAME": config('DB_NAME'),
+        "USER": config('DB_USER'),
+        "PASSWORD":config('DB_PASSWORD'),
+        "HOST": config('DB_HOST'),
         "PORT": "5432",
     }
 }
